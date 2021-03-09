a8d351e4553dd6ce9d9903a0da36561ac2d50c8e
absltn000 <78900100+absltn000@users.noreply.github.com>
2021-03-09 11:07:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,16 +1,16 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-	if (a < b){
-		int t=a;
-		a=b;
-		b=t;
+	if (a < b) {
+		int t = a;
+		a = b;
+		b = t;
 	}
-	while (b != 0){
+	while (b != 0) {
 		a = a % b;
-		int t=a;
-		a=b;
-		b=t
+		int t = a;
+		a = b;
+		b = t
 	}
 	return a;
 }
